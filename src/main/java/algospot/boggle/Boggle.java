3b5c949862ece41bc6d7696d0ac1bb64da315ac2
MilkTea24 <kimmanjae15@pusan.ac.kr>
2023-01-13 14:46:07 +0900

    2023.01.13

---
+++ src/main/java/algospot/boggle/Boggle.java
@@ -0,0 +1,57 @@
+package algospot.boggle;
+
+import java.util.Scanner;
+
+public class Boggle {
+    public static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        int cases = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < cases; i++) {
+            boggle();
+        }
+    }
+
+    public static void boggle() {
+        char[][] board = new char[5][5];
+        for (int i = 0; i < 5; i++) {
+            String inputLine = scanner.nextLine();
+            for (int j = 0; j < 5; j++) {
+                board[i][j] = inputLine.charAt(j);
+            }
+        }
+        int cases = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < cases; i++) {
+            String testString = scanner.nextLine();
+            String result = isExistAtBoard(testString, board);
+            System.out.println(testString + " " + result);
+        }
+    }
+
+    public static String isExistAtBoard(String target, char[][] board) {
+        int result = 0;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (target.charAt(0) == board[i][j]) {
+                    result = DFS(target, board, i, j, 1);
+                    if (result == 1) return "YES";
+                }
+            }
+        }
+
+        return "NO";
+    }
+
+    public static int DFS(String target, char[][] board, int i, int j, int index){
+        if (target.length() == index) return 1;
+        int result = 0;
+        if (i >= 1 && board[i-1][j] == target.charAt(index)) result = DFS(target, board, i-1, j, index + 1);
+        if (i >= 1 && j >= 1 && board[i-1][j-1] == target.charAt(index)) result = DFS(target, board, i-1, j-1, index + 1);
+        if (j >= 1 && board[i][j-1] == target.charAt(index)) result = DFS(target, board, i, j-1, index + 1);
+        if (i <= 3 && j >= 1 && board[i+1][j-1] == target.charAt(index)) result = DFS(target, board, i+1, j-1, index + 1);
+        if (i <= 3 && board[i+1][j] == target.charAt(index)) result = DFS(target, board, i+1, j, index + 1);
+        if (i <= 3 && j <= 3 && board[i+1][j+1] == target.charAt(index)) result = DFS(target, board, i+1, j+1, index + 1);
+        if (j <= 3 && board[i][j+1] == target.charAt(index)) result = DFS(target, board, i, j+1, index + 1);
+        if (i >= 1 && j <= 3 && board[i-1][j+1] == target.charAt(index)) result = DFS(target, board, i-1, j+1, index + 1);
+        return result;
+    }
+}
---
+++ src/main/java/example/ch06/sec03/BoggleExample.java
@@ -0,0 +1,63 @@
+package example.ch06.sec03;
+
+import java.util.Scanner;
+
+public class BoggleExample {
+    static final int[] dx = {-1, -1, -1, 1, 1, 1, 0, 0};
+    static final int[] dy = {-1, 0, 1, -1, 0, 1, -1, 1};
+    static char[][] board = new char[5][5];
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+
+        int testCases = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < testCases; i++) {
+            oneCase();
+        }
+    }
+
+    public static boolean hasWord(int y, int x, String word) {
+        if(!inRange(y, x)) return false; //범위 안인지 아닌지는 기저사례에서 처리 -> 내가 작성한 코드보다 간결한 코드
+        if(board[y][x] != word.charAt(0)) return false;
+        if(word.length() == 1) return true;
+        for (int direction = 0; direction < 8; ++direction) {
+            int nextY = y + dy[direction], nextX = x + dx[direction];
+            if(hasWord(nextY, nextX, word.substring(1)))
+                return true;
+        }
+        return false;
+    }
+
+    public static boolean inRange(int y, int x) {
+        if (y >= 0 && y < 5 && x >= 0 && x < 5) return true;
+        else return false;
+    }
+
+    public static void oneCase() throws Exception {
+        for (int i = 0; i < 5; i++) {
+            String line = scanner.nextLine();
+            for (int j = 0; j < 5; j++) {
+                board[i][j] = line.charAt(j);
+            }
+        }
+
+        int words = Integer.parseInt(scanner.nextLine());
+        for (int n = 0; n < words; n++) {
+            boolean result = false;
+            String word = scanner.nextLine();
+            Label: for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 5; j++) {
+                    if (hasWord(i, j, word)) {
+                        System.out.println(word + " YES");
+                        result = true;
+                        break Label;
+                    }
+                }
+            }
+
+            if (!result) System.out.println(word + " NO");
+        }
+    }
+
+
+}
